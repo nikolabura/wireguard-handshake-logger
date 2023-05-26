e3d2f53e6f1e1e53cf2ae1198a9cf11f63f7e999
Nikola <nikola@metactf.com>
2023-05-26 18:59:07 +0000

    Added Firezone API calls to annotate logs

--- wireguard-handshake-logger.sh
+++ wireguard-handshake-logger.sh
@@ -7,6 +7,8 @@ then
     runcmd="./bpftrace"
 fi
 
+seen_pubkeys=""
+
 IFS=$'\n'
 while read line
 do
@@ -18,7 +20,25 @@ do
     arr=($line)
     pubkey=$(echo ${arr[-1]} | xxd -r -p | base64)
     left_part=$(echo -n "$line" | sed "s/Pubkey.*//")
-    echo "$left_part" Pubkey: $pubkey
+    if [[ "$seen_pubkeys" == *"$pubkey"* ]]; then
+	    # pubkey has already been seen, use cached API
+	    :
+    else
+	    # pubkey wasn't seen before, hit up the API
+	    seen_pubkeys+=" $pubkey"
+	    api_devices=$(curl -s $(cat firezone_url.txt)/v0/devices -H 'Content-Type: application/json' -H "Authorization: Bearer $(cat token.txt)")
+	    api_users=$(curl   -s $(cat firezone_url.txt)/v0/users   -H 'Content-Type: application/json' -H "Authorization: Bearer $(cat token.txt)")
+    fi
+    # which device is this?
+    device_json=$(echo "$api_devices" | jq ".data[] | select(.public_key==\"$pubkey\")")
+    device_name=$(echo "$device_json" | jq -r ".name")
+    user_id=$(echo "$device_json" | jq -r ".user_id")
+    user_email=$(echo "$api_users" | jq -r ".data[] | select(.id==\"$user_id\") | .email")
+    if [ -z "${device_name}" ]; then
+	    user_email="UNKNOWN"
+	    device_name="UNKNOWN"
+    fi
+    echo "$left_part Pubkey: $pubkey       Email: $user_email      Device: $device_name"
 done < <($runcmd kprobe_wg_handshake.bt)
 
 echo Exiting Wireguard handshake logger.
